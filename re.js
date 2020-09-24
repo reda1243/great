b2f62a2d8134f0c2b94b6fa1929c99279a8be124
reda1243 <redachikhaoui12@gmail.com>
2020-09-24 12:30:40 +0200

    the new version

---
+++ re.js
@@ -0,0 +1,510 @@
+//   /*unction red(){
+//  *
+//  *  var yourAge = prompt("What Aour Age ?");
+//  *
+//  *     "use strict";
+//  *
+//  *  if(yourAge >= 20) {
+//  *
+//  *      alert("Your Age is "+ yourAge + " You Are Welcom" );
+//  *
+//  *      } else if(yourAge < 20){
+//  *
+//  *      alert("Your Age Is "+ yourAge + " You Are Not Allowd Here");
+//  *
+//  *  }  else{
+//  *
+//  *     alert("Sorry this is not number");
+//  *
+//  *  }
+//  * }
+//  * //end the fonction red
+//  *
+//  * // start calc your age
+//  *
+//  * function calcMe(age){
+//  *
+//  *     "use strict"
+//  *
+//  *     var year =365;
+//  *
+//  *     if (age < 10){
+//  *
+//  *         alert("You Are Very Young");
+//  *
+//  *     }else if (age > 10){
+//  *
+//  *         alert("Your Place Is Here Cous Your Age is " + age  );
+//  *     }
+//  *
+//  *     return   age * year;
+//  * }
+//  *
+//  * var  great = prompt("Pleas Enter Your Here");
+//  *
+//  * document.getElementById("test").innerHTML = calcMe(great);
+//  *
+
+// // end calc your age
+
+// (function wherYou() {
+//    var contry = prompt("Wher Are You From ?");
+
+//    switch (contry) {
+//       case "moroco":
+//          alert("I love Marakech");
+
+//          break;
+
+//       case "newyork":
+//           alert("is my pararfect city ");
+
+//          break;
+
+//       default:
+//          alert("You  Sould Write Something ");
+//    }
+// })();
+// var myFrend = [", "rim", "yassin", "rihan", "mohamed", "mustafa"];
+// myFrend.length()
+// console.log(specialFrend);*/
+
+//   //  /.//
+
+//   /*/function myInfo() {
+//      var first = prompt("first number ");
+//      var secend = prompt("secend number");
+
+//      var allthe = first - secend;
+//      alert(first + "+" + secend + "=" + allthe)
+
+//   }
+//   //start
+//   var theName = [
+//      "       raja",
+//      "fatiha",
+//      "mohamed",
+//      "rim",
+//      "
+//   ]
+
+//   var specialname = theName.indexOf(")
+
+//   console.log(theName.to)
+
+//   document.getElementById("all").innerHTML =
+
+//      "The name of my family is :" + theName.join(" | ");
+
+//   document.getElementById("special").innerHTML =
+
+//      "My special frend is : " + theName[specialname];
+
+//   //end
+
+//   var myname = "      ";
+//   console.log(myname.trim())
+
+//   console.log(myname.replace("r", "R").trim().toUpperCase().startsWith("R"))*
+
+//   //for  start
+//   /*for (i = 0; i <= 6; i++) {
+//      console.log(i)
+//   }
+//   //for end
+//   var i = 0;
+//   while (i <= 3) {
+//      console.log(i)
+//      i++
+//   }
+
+//   //while start
+
+//   //while end
+//   var i = o;
+//   while (i <= 7) {
+//      console.log(i)
+//      i++
+
+//   }
+//   //do while start
+//   var i = 0;
+//   do {
+//      console.log(i)
+//      i++;
+//   } while (i <= 7)
+
+//   //do while end
+//   var i,
+//      x;
+//   for (i = 0; i <= 7; i++) {
+//      for (x = 0; x <= 12; x++) {
+//         console.log(i + "=>" + x)
+
+//   }*/
+
+//   //   var caclyourage = alert("Click the botton to clac your age on days")
+
+//   //   function calc() {
+//   //      var EnteryourAge = prompt("enter your age "),
+//   //         coners = EnteryourAge * 365;
+//   //      document.getElementById("age").innerHTML =
+//   //         "your age on days is " + coners + "days";
+//   //      if (EnteryourAge >= 0) {
+//   //         console.error("not 0and under 0")
+//   //      }
+
+//   //   }
+//   //   let firstname = 'Chikhaoui'
+//   //   let secendname = ''
+//   //   console.log(firstname.indexOf("i")) //index of
+//   //   console.log(firstname.search("i")) //search
+//   //   console.log(firstname.repeat(2)) //repeat
+//   //   console.log(firstname.replace(firstname, 'loukrifi')) //replace
+//   //   console.log(firstname.slice(4, 6)) //slize
+//   //   console.log(firstname.substr(4, 6)); //slize
+
+//   //   function myInfo() {
+//   //      var numberone = prompt("first number ")
+//   //      numbertwo = prompt("secend number")
+//   //   numberone=Number(numberone);
+//   //   numbertwo=Number(numbertwo);
+
+//   //      alert(`the number one plus two is equal ${numberone + numbertwo}`)
+
+//   //   }
+//   //   var names = [", "yassine", "hassan"]
+//   //   for (i = 0; i < names.length; i++) {
+//   //      console.log(`nome ${ i + 1 } : ${names [ i ] }`)
+//   //   }
+//   //   var parassword = prompt('Ente your parassword')
+//   //   if (parassword.length < 8) {
+//   //      console.warn('The parassword should be great than 8 charaster')
+
+//   //   }else{
+//   //      console.log('good parasswor')
+//   //   }
+//   //   if (parassword.length >= 8) {
+//   //      console.log('Good parassword')
+//   //   }
+//   //   var note = prompt("Cheheale jabti")
+
+//   //   if (note == 20) {
+//   //      console.log('exelent')
+//   //   } else if (note > 20) {
+//   //      console.log('Your are not human')
+//   //   } else if (note >= 17) {
+//   //      console.log('very good')
+//   //   } else if (note >= 14) {
+//   //      console.log('good')
+//   //   } else if (note >= 10) {
+//   //      console.log('be curfl')
+//   //   } else if (note <= 9.99) {
+//   //      console.warn('sorry but you will repeat this years')
+//   //   }
+//   //   let email = 'chikha@'
+//   //   if (email.length < 8 || !email.includes("@")) {
+//   //      console.log('invalid')
+
+//   //   } else if (email.length > 8 || email.includes("@")) {
+//   //      console.log('valid')
+//   //   }
+//   //   var number = [13, 34, 33, 35, 43, 54, 18]
+//   //   for (i = 0; i < number.length; i++) {
+//   //      if (number[i] == 43) {
+//   //         continue;
+//   //      }
+//   //      console.log(`the score is ${ i+1 } : ${number[i]}`)
+
+//   //   }
+//   //   let age = 17;
+//   //   let result = (age >= 17) ? "kbir" : "s4ir";
+//   //   console.log(result)
+
+//   //   for (let x = 1048576; x > 1; x /= 2) {
+//   //      console.log(x)
+//   //   }
+
+//  /* const green = () => {
+//     var rayon = prompt('enter  le rayon');
+//     result = 3.13 * rayon;
+//     return result;
+//   }
+//   let surfa = green()
+//   console.log(surfa);
+
+//   let names = ["yassin", "mostaf", "reda", "reda2", "fatiha"]
+//   names.forEach(function (name, index) {
+//     console.log(`${ index } : ${ name }`)
+//   })
+
+//   //   let call = ["yassin", "mostaf", "reda", "reda2", "fatiha"],
+//   //      var person = (name, index) => {
+//   //         console.log(index + " : " + name)
+//   //      }
+//   //   call.forEach(person)
+//   var use = {
+//     myName: "Reda",
+//     middelName: "chikhaoui",
+//     age: 17,
+//     email: "redachikhaoui@gmail.com",
+//     name: ["reda", "yassine", 'rim'],
+//     login: function () {
+//       for (let i = 0; i < this.name.length; i++) {
+//         console.log(this.name[i])
+//       }
+//       this.name.forEach((names) => console.log(names))
+//     },
+//     mycours: [{
+//         nomes: "javascript",
+//         price: "299"
+//       },
+//       {
+//         nomes: "angular",
+//         price: "399"
+//       },
+//     ],
+//     cours: function () {
+//       this.mycours.forEach(nam => console.log(nam.price))
+//       for (i = 0; i < this.mycours.length; i += 1) {
+//         console.log(this.mycours[i])
+
+//       }
+//     },
+
+//     rjawi: ["hassan", "mohamed", "khalid"],
+
+//     //   this.rjawi.forEach(function (lesNomse, index) {
+//     //      console.log(index, lesNomse);
+//     //   })
+//     lesfr: function () {
+//       for (var i = 0; i < this.rjawi.length; i++) {
+//         console.log(this.rjawi[i])
+//       }
+//       this.rjawi.forEach(cour => console.log(cour))
+//       if (this.rjawi.length == 3) {
+//         console.log('The finsh')
+//       }
+
+//     },
+//   }
+//   // console.log(use.cours())
+//   // console.log(use.lesfr())
+//   // console.log(use.login())
+
+//   //Now We Ganna Start Learning DOM = Document Object Model
+
+//   const ph = document.querySelectorAll('p')
+
+//   ph.forEach(para => {
+//     if (para.innerText.includes('reda')) {
+//       para.classList.add('kayna')
+//     }
+//     if (para.innerText.includes('yass')) {
+//       para.classList.add('yass')
+//     }
+//   })
+//   var img = document.querySelector('#img')
+//   img.addEventListener('click', () => {
+//     img.remove()
+//   });
+
+//   var link = document.querySelector('.id')
+//   link.addEventListener('click', () => {
+//     console.log(`you click this button my frend`)
+//   });
+
+//   var para = document.querySelector('.click');
+//   var header = document.querySelector('.from');
+
+//   para.addEventListener('click', () => {
+//     const a = document.querySelector('a');
+//     a.textContent = ('#')
+//     header.prepend(a)
+//     a.style.color = "red"
+//     a.classList.add('green')
+//   });
+
+//   let num = prompt(`pleas enter numbet between 10 and 1`);
+//   num = Number(num);
+//   for (i = 1; i <= 10; i++) {
+//     if (num >= 1 && num <= 10) {
+//       console.log(`${ num } x ${ i } = ${ num * i}`)
+//     }
+//     console.log(``)
+//   };
+//   for (i = 11; i <= 90; i++) {
+//     if (i % 5 == 0) {
+//       console.log(`The number egul zero is ${ i }`)
+//     }
+//   };
+//   var nums = [13, 34, 56, 58];
+//   var s = 0;
+//   for (var i = 0; i < nums.length; i++) {
+//     s += nums[i]
+//     console.log(s);
+//   };
+//   console.log(this.s);
+//   var namess = [1, 3, 9, 5, 6, 7];
+//   console.log(namess);
+//   namess[1] = 2;
+//   console.log(namess);
+//   namess = namess.sort();
+//   namess.forEach(names => console.log(names))
+//   var notes = [{
+//       note: 12,
+//       nome: "fatiha"
+//     },
+//     {
+//       note: 20,
+//       nome: "reda"
+//     }
+//   ]
+//   console.log(notes)
+//   if (notes == 20) {
+//     console.log(`${note}${nome}`)
+//   }
+//   for (i = 0; i <= 4; i++) {
+//     for (j = 0; j <= i; j++) {
+//       console.log(``)
+//     }
+//     console.log(`n`)
+//}
+// let num = prompt(`pleas enter numbet between 10 and 1`);
+// num = Number(num);
+// for (i = 1; i <= 10; i++) {
+//   if (num >= 1 && num <= 10) {
+//     console.log(`${ num } x ${ i } = ${ num * i}`)
+//   }
+
+// }
+//this way is not pro why if she is local you can call globaly
+// var name = "Reda chikhoui";
+
+//let is most use cause she is not like var
+// let age = 18;
+//this is constant you can't change it
+// const from = "Moroco";
+//if you still use this shut your from another earth
+// mother = "fatiha";
+//and this for show in the console in inpect
+
+// let notNumber = "1232";
+
+// parseInt(notNumber);
+// console.log(notNumber);
+// //templat string
+// console.log(`the like is one like par second is : ${likes - notNumber}`);
+// let names = ["reda", 12, true];
+
+//delet the last file
+//The loop
+
+// let nome = ["faiha", "reda", "rim", "mostafa"];
+
+// let i = 0;
+// do {
+//   console.log(`the name for each one is :${nome[i]} ${i + 1}`);
+//   i++;
+// } while (i < nome.length);
+// console.log("the last line inn the looop");
+//if and else
+
+// let password = prompt("Please inter mote than 8 charchter");
+
+// if (password.length < 8 || !password.includes("@")) {
+//   // console.log("is good");
+//   console.error("not valid");
+// } else {
+//   console.log("is valid");
+// }
+// let num = [1, 35, 533, 53, 31, 23, 532, 52, 1, 532];
+// for (let i = 0; i < num.length; i++) {
+//   console.log(num[i]);
+//   if (num[i] == 1) {
+//     break
+//   }
+
+// }
+//condition tairner
+// let agee = 12;
+// let reslt = (15 > agee) ? "PETIT" : 'GRAND';
+// console.log(reslt)
+// let names = ["reda", "fatha", "yassin ", "lhaja"];
+
+// for (let i = 0; i < names.length; i++) {
+//     if (names[i] == "fatha") {
+//         console.log("not the mather");
+//         continue;
+//     }
+//     console.log(names[i]);
+// }
+//see the name is the function
+// nam = Number.age;
+
+// function yourName(age) {
+//   if (age >= 13) {
+//     console.log("minor");
+//   } else {
+//     console.log("your not allowd here");
+//   }
+// }
+// let youn = yourName(age);
+// console.log(youn)
+// let rayon = prompt("Pleas entre the rayon ");
+// let calc =  (rayon) => lrgeur = 12.2 * rayon;
+
+// calc(rayon)
+
+// console.log(lrgeur)
+// let name = 'kimwi123'
+// let fun =  name => console.log(`The name of the girl i wish stay with me is ${ name }`)
+// fun(name)
+
+// let num = [1, 35, 533, 53, 31, 23, 532, 52, 1, 532];
+// for (let i = 0; i < num.length; i += 2) {
+//     console.log(num[i])
+// }
+// let infof = prompt('what is your name')
+// let infos = prompt('what is your lastname')
+
+// console.log(info)
+// let writeNum = prompt('Pleas entre number between 1 and 10')
+// // writeNum = Number.writeNum
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${ writeNum } + ${ i } = ${writeNum * i } `)
+// }
+// let myinfo = [{
+//     name: "reda",
+//     lastName: "chikhaoui"
+// }];
+// let user = {
+//     courses: myinfo,
+
+//     findInfo: function () {
+//         this.courses.forEach(cours => {
+//             console.log(cours)
+//         })
+//     }
+// }
+// console.log(user.findInfo())
+let sum = 0;
+let i = 2;
+let evenSum = 0;
+let addSum = 0;
+while (i <= 10) {
+  sum = sum + i;
+  if (i % 2 == 0) {
+    evenSum = evenSum + i;
+  } else {
+    addSum = addSum + i;
+  }
+  i += 1;
+  console.log(i);
+}
+console.log(sum);
+console.log(evenSum);
+console.log(addSum);
+
+const divesion = document.querySelector('p')
+console.log(divesion.remove())
